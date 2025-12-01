5a385eabc64ad66626a2bbded790112736ce2e85
maithli21 <marora1_be24@thapar.edu>
2025-12-01 11:19:36 +0530

    Create q4.cpp

---
+++ LAB ASS 8/q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+bool bst ( TreeNode* root , TreeNode* min , TreeNode* max){
+  if ( root == NULL) return true;
+if( min != NULL && root -> val <= min -> val) return false;
+if( max != NULL && root -> val >= max -> val ) return false;
+return (bst(root-> left , min , root) && bst(root -> right , root , max));
+}
+    bool isValidBST(TreeNode* root) {
+        return bst(root , NULL ,NULL);    
+    }
+};
